0a941158b1efa0d1ec98befbcdf2cf66125d9ed8
Ajibolanle Gloria <37021798+Glowrare@users.noreply.github.com>
2022-03-13 19:03:52 -0400

    Updated footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,10 @@ import classes from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <p>Love Calculator</p>
+      <p>
+        Love Guage | &copy; 2022{' '}
+        <a href='https://github.com/Glowrare'>Ajibolanle Gloria</a>
+      </p>
     </footer>
   );
 };
